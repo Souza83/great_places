f45516bd2f244437c78b2979a332c4b1448eff27
Souza83 <tec.psouza@gmail.com>
2022-07-27 06:42:19 -0300

    Submetendo Formulário.

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/grate_places.dart';
 import 'package:great_places/widgets/image_input.dart';
+import 'package:provider/provider.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({Key? key}) : super(key: key);
@@ -18,7 +20,18 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
     pickedImage = pickedImage;
   }
 
-  void _submitForm() {}
+  void _submitForm() {
+    if (_titleController.text.isEmpty || pickedImage == null) {
+      return;
+    }
+
+    Provider.of<GreatPlaces>(context, listen: false).addPlace(
+      _titleController.text,
+      pickedImage!,
+    );
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
