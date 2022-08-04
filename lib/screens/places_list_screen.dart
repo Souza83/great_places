f360b66a2ff6bae68115b50b6b8941588677f624
Souza83 <tec.psouza@gmail.com>
2022-08-04 05:31:37 -0300

    Usando SQLite #01.

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
 import 'package:provider/provider.dart';
-
-import '../providers/grate_places.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key? key}) : super(key: key);
