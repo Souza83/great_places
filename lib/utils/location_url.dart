bde9fc223ac78e794cf2c73ecc37f8f5c7831d6d
Souza83 <tec.psouza@gmail.com>
2022-08-13 05:44:20 -0300

    Mostrando Imagem Estática do Mapa.

--- lib/utils/location_url.dart
+++ lib/utils/location_url.dart
@@ -1,10 +1,11 @@
 const GOOGLE_API_KEY = 'AIzaSyAeCaEDB8Ukhpj_46o49fyj4hOATAvwhAs';
 
-class LocationUtil{
+class LocationUtil {
   static String generateLocationPreviewImage({
-    double ?latitude,
-    double ?longitude,
-  }){ // Procure no goglle por google maps static api e copie exemplo
+    double? latitude,
+    double? longitude,
+  }) {
+    // Procure no goglle por google maps static api e copie exemplo
     return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=GOOGLE_API_KEY';
   }
 }
