184c9788095b344cf866cf0555068ed5ba222bfb
Souza83 <tec.psouza@gmail.com>
2022-08-23 04:57:25 -0300

    Tela de Detalhe do Lugar e conclusão do app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/great_places.dart';
+import 'package:great_places/screens/place_detail_screen.dart';
 import 'package:great_places/screens/place_form_screen.dart';
 import 'package:great_places/screens/places_list_screen.dart';
 import 'package:great_places/utils/app_routes.dart';
@@ -17,7 +18,6 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (ctx) => GreatPlaces(),
       child: MaterialApp(
-        
         debugShowCheckedModeBanner: false,
         title: 'Great Places',
         theme: ThemeData(
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
         home: const PlacesListScreen(),
         routes: {
           AppRoutes.placeForm: (ctx) => const PlaceFormScreen(),
+          AppRoutes.placeDetail: (ctx) => const PlaceDetailScreen(),
         },
       ),
     );
